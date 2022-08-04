8754b42440400abc0e3d9176fe11e0bcfa23dcb3
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2022-08-04 22:37:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+$("img").animate({ opacity: 0 });
+setTimeout(function () {
+    $("img").animate({ opacity: 1.5 });
+}, 500);
+
+// $("body").on("scroll", function () {
+//     var posY = element.getBoundingClientRect().top;
+//     if (posY > 200) {
+//         $('.floating').addClass('past-main');
+//     } else {
+//         $('.floating').removeClass('past-main');
+//     }
+// });
+
